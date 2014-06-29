093acf6aab1cd3885ef6c46ca1f68770e1767668
pantagrel <kristin.gannon@gmail.com>
2014-06-28 21:13:08 -0700

    got all current files into new nav design, removed some old chaff.

--- code-kriyas/04_materializing-shapes.php
+++ code-kriyas/04_materializing-shapes.php
@@ -1,31 +1,25 @@
-<html>
-<head>
-	<title>Materializing Shapes</title>
-	<link rel="stylesheet" type="text/css" href="_css/style.css">
-	<script type="text/javascript" src="http://code.createjs.com/easeljs-0.7.1.min.js"></script>
-	<script type="text/javascript" src="_js/materializing-shapes.js"></script>
-	<style type="text/css">
-		body{
-			background-color: #aaa;
-		}
-
-		canvas, #canvas{
-			background-color: #bbb;
-		}
-	</style>
-</head>
-
-<body onload="init();">
-	<canvas id="canvas" width="500" height="500">
-		<p>If you can see this message, your browser doesn't support canvas.
-		</p>
-		<p>
-		Your browser should support canvas! Consider trying Chrome or Firefox.
-		</p>
-	</canvas>
-	<p>
-		I only cherish some routines. However, I suspect there may be useful clues in all of them. 
-	</p>
-	<p>&#8617; <a href="../">Index</a></p>
-</body>
-</html>
+	<html>
+		<head>
+			<title>Materializing Shapes</title>
+			<script type="text/javascript" src="//use.typekit.net/gle3hly.js"></script>
+			<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+			<link rel="stylesheet" type="text/css" href="../_css/topLevelStyle.css">
+			<script type="text/javascript" src="http://code.createjs.com/easeljs-0.7.1.min.js"></script>
+			<script type="text/javascript" src="_js/materializing-shapes.js"></script>
+		</head>
+		
+		<body onload="init();">
+			<?php
+				include '../_includes/kriyas-header.php';
+			?>
+			<div id="kriya">
+				<canvas id="canvas" width="500" height="500">
+				<p>If you can see this message, your browser doesn't support canvas.
+				</p>
+				</canvas>
+				<p>
+					I only cherish some routines. However, I suspect there may be useful clues in all of them. 
+				</p>
+			</div><!-- kriya -->
+	</body>
+	</html>
