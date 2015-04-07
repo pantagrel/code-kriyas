2bb3154c0ee5fc8a0faf70f2143489f4bfbff7a8
pantagrel <kristin.gannon@gmail.com>
2015-04-07 05:26:02 -0700

    added some videos to the mix

--- _includes/video_variables.php
+++ _includes/video_variables.php
@@ -8,11 +8,41 @@
 		'video2' =>
 		'<video src="../_img/_video/ocean1_1.mp4" preload="auto" autoplay >
 	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video3' =>
+		'<video src="../_img/_video/grass.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video4' =>
+		'<video src="../_img/_video/grass2.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video5' =>
+		'<video src="../_img/_video/grass3.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',		
+		'video6' =>
+		'<video src="../_img/_video/flowers1.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video7' =>
+		'<video src="../_img/_video/flowers2.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video8' =>
+		'<video src="../_img/_video/fan.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
+		</video>',
+		'video9' =>
+		'<video src="../_img/_video/loganPass1.mp4" preload="auto" autoplay >
+	  		Your browser does not support the <code>video</code> element.
 		</video>'
 	)		
 
 	//pull down list of video files with php
-	//load and auto play all files with javascript
+	//get list.
+	//rearrange randomly (weighted with a short light one first)
+	//then auto play all files with javascript
 
 
 ?>
