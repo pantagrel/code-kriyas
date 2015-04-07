5354bcaf022eda6cdbde84416db8722bc281d490
pantagrel <kristin.gannon@gmail.com>
2015-04-07 05:48:01 -0700

    canvas overlay

--- about.php
+++ about.php
@@ -4,10 +4,24 @@
 	<script type="text/javascript" src="//use.typekit.net/gle3hly.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<link rel="stylesheet" type="text/css" href="_css/topLevelStyle.css">
+
+	<script src="https://code.createjs.com/easeljs-0.8.0.min.js"></script>
+
+	<script>
+  function init() {
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	stage.update();
+  }
+</script>
 	
 </head>
 
-<body>
+<body onload="init();">
 	
 	<nav>
 			<div id="topNav">
@@ -25,11 +39,21 @@
 
 
 	<div id="about_page">
+		<p class="video">
+			BLUSTER.
+		</p>
+		<canvas id="demoCanvas""></canvas>
 		<?php
 			// echo about_text();
 			echo random_video();
 			// echo '<p>.</p><p>'.$_SERVER['HTTP_USER_AGENT'].'</p>';
 		?>
+
+		
+
+		<script>
+			console.log();
+		</script>
 	</div><!-- about_page -->
 
 </body>
